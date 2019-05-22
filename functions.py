79662c27292d7e83db9f8e04a0701c5b99d1de7b
mlk1337 <morris.klasen@gmail.com>
2019-05-22 10:51:11 +0200

    m

---
+++ functions.py
@@ -0,0 +1,52 @@
+import numpy as np
+import math
+
+v = 300
+q = 9
+w = q/(2*v)
+A = (v*v)/q
+
+def calcPosition(i):
+    x = A * np.sin(w*i)
+    y = A * np.sin(2*w*i)
+    i = [x,y]
+    return i
+
+def calcVelocity(i):
+    t = i
+    x = v * (np.cos(w*t)/2)
+    y = v * np.cos(2*w*t)
+    vec = [x,y]
+    return vec
+
+def calcAcceleration(t):
+    x = (-q) * (np.sin(w*t)/4)
+    y = (-q) * np.sin(2*w*t)
+    vec = [x,y]
+    return vec
+
+def calc_tangent(t):
+    x= v*math.cos(w*t)/2
+    y= v*math.cos(2*w*t)
+    return [x/math.sqrt(x*x+y*y),y/math.sqrt(x*x+y*y)]
+
+def calc_normal(t):
+    x= -(v*math.cos(2*w*t))
+    y= v*math.cos(w*t)/2
+    return [x/math.sqrt(x*x+y*y),y/math.sqrt(x*x+y*y)]
+
+def abs_velo(t):
+    [x,y] = calcVelocity(t)
+    return math.sqrt(x*x+y*y)
+
+def abs_accel(t):
+    [x,y] = calcAcceleration(t)
+    return math.sqrt(x*x+y*y)
+
+def accel_tangent(t):
+    [x,y] = calcAcceleration(t)
+    return [x*calc_tangent(t)[0],y*calc_tangent(t)[1]]
+
+def accel_normal(t):    
+    [x,y] = calcAcceleration(t)
+    return [x*calc_normal(t)[0],y*calc_normal(t)[1]]
